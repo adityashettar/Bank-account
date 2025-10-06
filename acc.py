2e70f0c5ddf6dfb36f2795c5089dcd84671b4332
adityashettar <adityashettar01@gmail.com>
2025-10-06 17:15:35 +0530

    save this file

---
+++ acc.py
@@ -0,0 +1,11 @@
+
+initial_balance = float(input("Enter the initial balance: "))
+
+
+deposit_amount = float(input("Enter the deposit amount: "))
+
+
+updated_balance = initial_balance + deposit_amount
+
+
+print("Updated balance after deposit:", updated_balance)
